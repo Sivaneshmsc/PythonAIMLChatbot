2c2113e46bc7734d5f19016202e7684c859aefc7
Thejashwini Sivanesh <sivaneshmsc@Gmail.com>
2020-05-24 07:32:42 +0530

    Python AIML Chatbot files

---
+++ Chatbot.py
@@ -0,0 +1,20 @@
+import aiml
+import os
+
+kernel = aiml.Kernel()
+
+if os.path.isfile("bot_brain.brn"):
+    kernel.bootstrap(brainFile = "bot_brain.brn")
+else:
+    kernel.bootstrap(learnFiles = os.path.abspath("brain.aiml"), commands = "load aiml b")
+    kernel.saveBrain("bot_brain.brn")
+
+while True:
+    message = input("Enter your message to the bot: ")
+    if message == "quit":
+        exit()
+    elif message == "save":
+        kernel.saveBrain("bot_brain.brn")
+    else:
+        bot_response = kernel.respond(message)
+        print(bot_response)
